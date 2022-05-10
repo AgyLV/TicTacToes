7e6a41ff4166fa1fb850b8f544c721f0d0b8d4f2
Gundars Eimanis <gundarens@gmail.com>
2022-05-10 22:08:09 +0300

    Gundars iesāk

--- TicTacToes/Program.cs
+++ TicTacToes/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("TicTacToes");
+Console.ReadLine();
+
+
+Console.WriteLine("What is the name of player 1?");
+string player1 = Console.ReadLine();
+Console.WriteLine("What is the name of player 2?");
+string player2 = Console.ReadLine();
+
+Console.WriteLine($"{player1} izvelies savu simbolu X vai O ");
+string player1Simbol = Console.ReadLine();
+
+if (player1Simbol == "O")
+{
+    string player2Simbol = "X";
+}
+else string player2Simbol = "O";
+
+Console.WriteLine($"{player1} simbols ir: {player1Simbol}");
+Console.WriteLine($"{player2} simbols ir: {player2Simbol}");
+
+
+
+
