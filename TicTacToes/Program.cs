5443c7dda01a1a78e7cdcabd7d5df832a0f730c0
Gundars Eimanis <gundarens@gmail.com>
2022-05-10 22:13:41 +0300

    changes

--- TicTacToes/Program.cs
+++ TicTacToes/Program.cs
@@ -7,7 +7,7 @@ string player1 = Console.ReadLine();
 Console.WriteLine("What is the name of player 2?");
 string player2 = Console.ReadLine();
 
-Console.WriteLine($"{player1} izvelies savu simbolu X vai O ");
+Console.WriteLine($"{player1} choose your simbol X or O ");
 string player1Simbol = Console.ReadLine();
 
 if (player1Simbol == "O")
@@ -16,8 +16,8 @@ if (player1Simbol == "O")
 }
 else string player2Simbol = "O";
 
-Console.WriteLine($"{player1} simbols ir: {player1Simbol}");
-Console.WriteLine($"{player2} simbols ir: {player2Simbol}");
+Console.WriteLine($"{player1} simbol: {player1Simbol}");
+Console.WriteLine($"{player2} simbol: {player2Simbol}");
 
 
 
